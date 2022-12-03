89284806dcb9e4b6ce5ca1093bfdb91957c8106b
Gourang-pro <117590608+Gourang-pro@users.noreply.github.com>
2022-12-03 21:25:31 +0530

    Add files via upload

---
+++ tables.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+        int i,n;
+    printf("ENTER ANY NUMBER :");
+    scanf("%d",&n);
+
+
+        for(i=0;i<=10;i++)
+        {
+            printf("%d*%d=%d \n",n,i,n*i);
+
+        }
+        
+
+
+    return 0;
+}
